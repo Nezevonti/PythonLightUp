be0c7da72e330e7f00c251c66e5fd75aeeee6207
Nezevonti <nezevonti@gmail.com>
2019-04-14 02:47:05 +0200

    Update PythonLightUp.py

--- PythonLightUp.py
+++ PythonLightUp.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from copy import deepcopy
 
 class Tile(Enum):
     Block = "b-"
@@ -16,12 +17,16 @@ class GameMap(object):
 
     def __init__(self,iSizeIn):
         self.Map = []
+        self.PointMap = []
         self.iSize = iSizeIn
         for i in range(0,self.iSize):
             x = []
+            x_points = []
             for j in range(0,self.iSize):
                 x.append(Tile.EmptyUnlit)
+                x_points.append(250)
             self.Map.append(x)
+            self.PointMap.append(x_points)
     
     def CopyMap(self,GMap):
         if(self.iSize != GMap.iSize):
@@ -30,6 +35,9 @@ class GameMap(object):
         else:
             for i in range(0,self.iSize):
                     for j in range(0,self.iSize):
+
+                        self.PointMap[i][j] = GMap.PointMap[i][j]
+
                         if(GMap.Map[i][j] == Tile.Block):
                             self.Map[i][j] = Tile.Block
                         elif(GMap.Map[i][j]== Tile.Block0):
@@ -48,9 +56,10 @@ class GameMap(object):
                             self.Map[i][j]=Tile.EmptyLit
                         elif(GMap.Map[i][j]== Tile.EmptyUnlit):
                             self.Map[i][j]=Tile.EmptyUnlit
-                        #print(self.Map[i][j].value,end="")
-                    #print("")
 
+    #
+
+    #Print
 
     def PrintMapConsole(self):
         for i in range(0,self.iSize):
@@ -58,7 +67,21 @@ class GameMap(object):
                 print(self.Map[i][j].value,end="")
                 print(" ",end="")
             print("")
-    
+
+    def PrintPointConsole(self):
+        for i in range(0,self.iSize):
+            for j in range(0,self.iSize):
+                print(self.PointMap[i][j],end="")
+                if(self.PointMap[i][j] < 10):
+                    print(" ",end="")
+                
+                if(self.PointMap[i][j] < 100):
+                    print(" ",end="")
+
+                print(" ",end="")
+            print("")
+
+
     def LoadMap(self,sPath):
         print("Loading from file " + sPath)
 
@@ -101,7 +124,15 @@ class GameMap(object):
             print("cannot place lamps in lit spaces...")
             return
 
-        print("Placing lamp at " + str(PosRow) +","+ str(PosCollumn) + " ...")
+        if(self.IsBlock(PosRow,PosCollumn)):
+            print("cannot place lamps on blocks...")
+            return
+
+        if(self.Map[PosRow][PosCollumn] == Tile.EmptyLamp):
+            print("cannot place lamps on lamps...")
+            return
+
+        #print("Placing lamp at " + str(PosRow) +","+ str(PosCollumn) + " ...")
         self.Map[PosRow][PosCollumn] = Tile.EmptyLamp
         #update lighting
 
@@ -140,6 +171,81 @@ class GameMap(object):
                 break
             else:
                 self.Map[PosRow][PosCollumn+j] = Tile.EmptyLit
+    
+    def UpdatePointMap(self):
+        print("Updating Point Map...")
+
+        #set blocks, neighbours of 0 and lit places to max
+        for row in range(0,self.iSize):
+            for collumn in range(0,self.iSize):
+                if(self.IsBlock(row,collumn)):
+                    self.PointMap[row][collumn] = 256
+
+                    if(self.Map[row][collumn]==Tile.Block0):
+                        if(row>0):
+                            self.PointMap[row-1][collumn] = 256
+                        if(row+1<self.iSize):
+                            self.PointMap[row+1][collumn] = 256
+                        if(collumn>0):
+                            self.PointMap[row][collumn-1] = 256
+                        if(collumn+1<self.iSize):
+                            self.PointMap[row][collumn+1] = 256
+
+                if(self.Map[row][collumn] == Tile.EmptyLit or self.Map[row][collumn] == Tile.EmptyLamp):
+                    self.PointMap[row][collumn] = 256
+
+        #check for numbered blocks neighbours
+        for rowB in range(0,self.iSize):
+            for collumnB in range(0,self.iSize):
+                if(self.IsBlock(rowB,collumnB)):
+                    if(self.Map[rowB][collumnB] == Tile.Block0 or self.Map[rowB][collumnB] == Tile.Block):
+                        continue
+                    A=0 #aviable neighbours to this block
+                    if(rowB>0):
+                        if(self.PointMap[rowB-1][collumnB] < 256):
+                            A=A+1
+                    if(rowB+1<self.iSize):
+                        if(self.PointMap[rowB+1][collumnB] < 256):
+                            A=A+1
+                    if(collumnB>0):
+                        if(self.PointMap[rowB][collumnB-1] < 256):
+                            A=A+1
+                    if(collumnB+1<self.iSize):
+                        if(self.PointMap[rowB][collumnB+1] < 256):
+                            A=A+1
+
+                    if(self.Map[rowB][collumnB] == Tile.Block1):
+                        X = 1
+
+                    if(self.Map[rowB][collumnB] == Tile.Block2):
+                        X = 2
+
+                    if(self.Map[rowB][collumnB] == Tile.Block3):
+                        X = 3
+
+                    if(self.Map[rowB][collumnB] == Tile.Block4):
+                        X = 4
+
+                    P = 25*(A-X)
+
+                    if(rowB>0):
+                        if(self.PointMap[rowB-1][collumnB] < 256):
+                            self.PointMap[rowB-1][collumnB] = P
+                    if(rowB+1<self.iSize):
+                        if(self.PointMap[rowB+1][collumnB] < 256):
+                            self.PointMap[rowB+1][collumnB] = P
+                    if(collumnB>0):
+                        if(self.PointMap[rowB][collumnB-1] < 256):
+                            self.PointMap[rowB][collumnB-1] = P
+                    if(collumnB+1<self.iSize):
+                        if(self.PointMap[rowB][collumnB+1] < 256):
+                            self.PointMap[rowB][collumnB+1] = P
+    
+
+    #
+
+    #Check Map
+
 
     def CheckMap(self):
         #print("Checking map...")
@@ -297,31 +403,140 @@ class GameMap(object):
             return True
         else:
             return False
-#Node
+
+
+# Node
 
 class Node():
     state = None
     parent = None
     nodesToVisit = []
     steps = None
-    def __init__(self,state,parent,steps):
+    visited = False
+
+    def __init__(self, state, parent, steps):
         self.state = state
         self.parent = parent
         self.steps = steps
-    
-    def UpdateNodesToVisit(self):  #Push all available moves to the stack
-        for y in range(0,6):
-            for x in range(0,6):
-                if(self.state.Map[y][x] == Tile.EmptyUnlit):  #if the move is valid
-                    temp = GameMap(7)
-                    temp.CopyMap(self.state)
-                    temp.PrintMapConsole()
-                    temp.PlaceLamp(y,x)                       #Modify current state
-                    self.nodesToVisit.append(temp)
 
     
+    def UpdateNodesToVisitA(self):  # Push all available moves to the stack
+        self.nodesToVisit= []
+        self.state.UpdatePointMap()
+
+        tile_points = []
+
+        def custom_sort(t):
+            return t[1]
+
+        for i in range(0,49):
+            tmp = []
+            tmp.append(i)
+            tmp.append(self.state.PointMap[(int)(i/7)][i%7])
+            tile_points.append(tmp)
+
+        tile_points.sort(key=custom_sort)
+
+        for g in range(len(tile_points)):
+            print(str(tile_points[g][0]) + " " + str(tile_points[g][1]))
+
+        for h in range(0,49):
+            k = tile_points[h][0]
+            if self.state.Map[(int)(k/7)][k%7] == Tile.EmptyUnlit:  # if the move is valid
+                temp = Node(self.state, self.parent, self.steps)
+                temp.state = deepcopy(self.state)
+                temp.parent = deepcopy(self.parent)
+                temp.steps = deepcopy(self.steps)
+                temp.state.PlaceLamp((int)(k/7),k%7)  # Modify current state
+
+                print()
+                temp.state.PrintMapConsole()
+                print()
+
+                self.nodesToVisit.append(temp)
+        
+    
+    #
+
+    #DF
+
+    def UpdateNodesToVisit(self):  # Push all available moves to the stack
+        self.nodesToVisit= []
+        for y in range(0, 7):
+            for x in range(0, 7):
+                if self.state.Map[y][x] == Tile.EmptyUnlit:  # if the move is valid
+                    temp = Node(self.state, self.parent, self.steps)
+                    temp.state = deepcopy(self.state)
+                    temp.parent = deepcopy(self.parent)
+                    temp.steps = deepcopy(self.steps)
+                    temp.state.PlaceLamp(y, x)  # Modify current state
+
+                    #print()
+                    #temp.state.PrintMapConsole()
+                    #print()
+
+                    self.nodesToVisit.append(temp)
+    
+#
+
+
+
+def depthfirstsearch(node):
+    # initial
+    root = Node(node.state, node.parent, node.steps)
+    root.UpdateNodesToVisit()
+    visited = []
+    a = root.nodesToVisit
+
+    print()
+    root.state.PrintMapConsole()
+    print()
+
+    # main loop
+    while a:
+        x = a.pop()
+        x.UpdateNodesToVisit()
+        if x.state.CheckMap():
+            return x
+        # check for visited states
+        flag = 0
+        for r in visited:
+            for p in x.nodesToVisit:
+                if p.state == r.state:
+                    flag = 1
+                    print("flag")
+        # if states were not visited append to stack
+        if flag == 0:
+            visited.append(x)
+            for n in x.nodesToVisit:
+                a.append(n)
+        
+
+#A*
+
+def Astar(node):
+    print("A* based solver")
+
+    root = Node(node.state,Node.parent,Node.steps)
+    root.UpdateNodesToVisitA()
+    visited = []
+    a = root.nodesToVisit
+
+    #root.state.UpdatePointMap()
+
+    while a:
+        x=a.pop()
+        x.UpdateNodesToVisitA()
+
+        #x.state.UpdatePointMap()
+
+        if x.state.CheckMap():
+            return x
+        
+        a = x.nodesToVisit
 
 #MainGameLoop
+
 
 def GameLoop():
     print("loop start...")
@@ -367,32 +582,23 @@ def main():
     Map = GameMap(7)
     Map.LoadMap("TestFile.txt")
     Map.PrintMapConsole()
+    Map.UpdatePointMap()
+    Map.PrintPointConsole()
 
-    #GameLoop()
+
+
+
+#GameLoop()
+
     
 def Test():
-    Map1 = GameMap(7)
-    Map1.LoadMap("TestFile.txt")
-    Map1.PrintMapConsole()
-    print("---------")
-    Map2 = GameMap(7)
-    Map2.CopyMap(Map1)
-    Map2.PrintMapConsole()
-    Map2.PlaceLamp(0,2)
-
-    print("---------")
-    Map1.PrintMapConsole()
-    print("----\n---------\n---")
-    Map2.PrintMapConsole()
-    #Map.PrintMap()
-    #root = Node(Map,Map,Map)
-    #root.state.PrintMapConsole()
-    #root.UpdateNodesToVisit()
-    #n=len(root.nodesToVisit)
-    #print(n)
+    map = GameMap(7)
+    map.LoadMap("TestFile.txt")
+    map.PrintMapConsole()
+    root = Node(map,map,map)
+    d = Astar(root)
+    d.state.PrintMapConsole()
 
 Test()
 
-
 #main()
-
